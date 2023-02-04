d1a18c021c8cadf9d9c290707933caeccb50ac24
Chekkal Youcef <chekkalislam@gmail.com>
2023-02-04 19:08:42 +0100

    working on search

--- pages/search.js
+++ pages/search.js
@@ -15,12 +15,32 @@ const client = algoliasearch(
   process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY
 );
 
+function Product({hit}) {
+  return (
+    <pre> {JSON.stringify(hit, null, 2)} </pre>
+  )
+}
+
 
 
 const search = ({ hit }) => {
     console.log(hit)
   return (
-    <div>search</div>
+    <>
+    <h1>Search Products</h1>
+    <InstantSearch searchClient={client} indexName='products' > 
+    <Hits hitComponent={Product} />
+    {/* <ToggleRefinement
+    attribute={has.digital_delivery}
+    label={Digitaldeliveryonly}
+    value={string | number | boolean}
+    // Optional parameters
+    defaultRefinement={boolean}
+    /> */}
+    </InstantSearch> 
+
+      
+    </>
   )
 }
 
